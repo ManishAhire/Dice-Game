6563a9e46eb55a477ad5963ce4f7fa0cef2eaf35
Manish Ahire <ahiremanishb@gmail.com>
2022-01-08 01:12:00 +0530

    Initial Commit

--- Dice Game/ViewController.swift
+++ Dice Game/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    //MARK:- Outlets
+    @IBOutlet weak var imgFirstDice: UIImageView!
+    @IBOutlet weak var imgSecondDice: UIImageView!
+    
+    //MARK:- Action on button
+    @IBAction func rollButtonTapped(_ sender: UIButton) {
+        
+        /** Here we created an array of strings.
+         Used a let keyword - to make it constant so no one can change it
+         */
+        let diceImageNames = ["DiceOne", "DiceTwo", "DiceThree", "DiceFour", "DiceFive", "DiceSix"]
+        
+        /**
+         We used randomElement() function of array - Returns a random element of the collection.
+         So here it returns the name of an image from the diceImageNames array.
+         randomElement() returns an optional string so here we used ?? (Nil-Coalescing Operator) and give default value "DiceOne" if the randomElement() return nil/empty.
+         Nil-Coalescing Operator - unwraps an optional
+         */
+        
+        imgFirstDice.image = UIImage(named: diceImageNames.randomElement() ?? "DiceOne")
+        imgSecondDice.image = UIImage(named: diceImageNames.randomElement() ?? "DiceOne")
     }
-
-
 }
 
